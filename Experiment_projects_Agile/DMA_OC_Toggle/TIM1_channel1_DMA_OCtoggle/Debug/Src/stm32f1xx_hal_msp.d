b195bb0d7b942c4fc99e0b41fc0ad592cacb78be
YLoiSiah <louisesiah19@gmail.com>
2017-07-11 17:16:03 +0800

    OC Toggle mode and OC Toggle DMA mode edit

--- Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Debug/Src/stm32f1xx_hal_msp.d
+++ Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Debug/Src/stm32f1xx_hal_msp.d
@@ -1,73 +1,73 @@
 Src/stm32f1xx_hal_msp.o: ../Src/stm32f1xx_hal_msp.c \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Inc/stm32f1xx_hal_conf.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Inc/main.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Include/core_cm3.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Include/core_cmInstr.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Include/cmsis_gcc.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Include/core_cmFunc.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
- C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Inc/stm32f1xx_hal_conf.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Inc/main.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Include/core_cm3.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h \
+ C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Inc/stm32f1xx_hal_conf.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Inc/stm32f1xx_hal_conf.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Inc/main.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Inc/main.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_def.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f1xx.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Device/ST/STM32F1xx/Include/stm32f103xb.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Include/core_cm3.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Include/core_cm3.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Include/core_cmInstr.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Include/core_cmInstr.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Include/cmsis_gcc.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Include/cmsis_gcc.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Include/core_cmFunc.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Include/core_cmFunc.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/CMSIS/Device/ST/STM32F1xx/Include/system_stm32f1xx.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_rcc_ex.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_gpio_ex.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_dma_ex.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_cortex.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_flash_ex.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_pwr.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim.h:
 
-C:/SystemWorkbench/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
+C:/Users/user2/Desktop/STM8_Debugger/Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Drivers/STM32F1xx_HAL_Driver/Inc/stm32f1xx_hal_tim_ex.h:
--- Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Src/main.c
+++ Experiment_projects_Agile/DMA_OC_Toggle/TIM1_channel1_DMA_OCtoggle/Src/main.c
@@ -50,18 +50,19 @@ DMA_HandleTypeDef hdma_tim1_ch1;
 /* USER CODE BEGIN PV */
 /* Private variables ---------------------------------------------------------*/
 /* Timer handler declaration */
-TIM_HandleTypeDef    TimHandle;
+TIM_HandleTypeDef TimHandle;
 
 /* Timer Output Compare Configuration Structure declaration */
 TIM_OC_InitTypeDef sConfig;
 
 /* Capture Compare buffer */
-uint32_t aCCValue_Buffer[3] = {0, 0, 0};
+uint32_t aCCValue_Buffer[4] = { 0, 0, 0, 0 };
 
 /* Timer Period*/
-uint32_t uhTimerPeriod  = 0;
+uint32_t uhTimerPeriod = 0;
 
-
+uint32_t counter = 0;
+uint32_t uhCapture = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -110,57 +111,56 @@ int main(void)
   MX_TIM1_Init();
 
   /* USER CODE BEGIN 2 */
-  /* Compute the value of ARR regiter to generate signal frequency at 17.57 Khz */
-     uhTimerPeriod = (uint32_t) ((SystemCoreClock / 10000 ) - 1);
-     /* Compute CCR1 value to generate a duty cycle at 75% */
-     aCCValue_Buffer[0] = (uint32_t)(((uint32_t) 25 * (uhTimerPeriod - 1)) / 100);
-     /* Compute CCR2 value to generate a duty cycle at 50% */
-     aCCValue_Buffer[1] = (uint32_t)(((uint32_t) 75 * (uhTimerPeriod - 1)) / 100);
-     /* Compute CCR3 value to generate a duty cycle at 25% */
-     aCCValue_Buffer[2] = (uint32_t)(((uint32_t) 25 * (uhTimerPeriod - 1)) / 100);
+	/* Compute the value of ARR regiter to generate signal frequency at 17.57 Khz */
+	uhTimerPeriod = (uint32_t) ((SystemCoreClock / 10000) - 1);
+	/* Compute CCR1 value to generate a duty cycle at 75% */
+	aCCValue_Buffer[0] =  //8999;
+			(uint32_t) (((uint32_t) 10 * (uhTimerPeriod - 1)) / 100);
+	aCCValue_Buffer[1] = //17998;
+			(uint32_t) (((uint32_t) 20 * (uhTimerPeriod - 1)) / 100);
+	aCCValue_Buffer[2] = //17998;
+			(uint32_t) (((uint32_t) 30 * (uhTimerPeriod - 1)) / 100);
+	aCCValue_Buffer[3] = //17998;
+				(uint32_t) (((uint32_t) 40 * (uhTimerPeriod - 1)) / 100);
 
-     TimHandle.Instance = TIM1;
+	TimHandle.Instance = TIM1;
 
-         TimHandle.Init.Period            = uhTimerPeriod;
-         TimHandle.Init.RepetitionCounter = 0;
-         TimHandle.Init.Prescaler         = 0;
-         TimHandle.Init.ClockDivision     = 0;
-         TimHandle.Init.CounterMode       = TIM_COUNTERMODE_UP;
-         if(HAL_TIM_OC_Init(&TimHandle) != HAL_OK)
-         {
-           /* Initialization Error */
-           Error_Handler();
-         }
+	TimHandle.Init.Period = uhTimerPeriod;
+	TimHandle.Init.RepetitionCounter = 0;
+	TimHandle.Init.Prescaler = 0;
+	TimHandle.Init.ClockDivision = 0;
+	TimHandle.Init.CounterMode = TIM_COUNTERMODE_UP;
+	if (HAL_TIM_OC_Init(&TimHandle) != HAL_OK) {
+		/* Initialization Error */
+		Error_Handler();
+	}
 
-         /*##-2- Configure the PWM channel 3 ########################################*/
-         sConfig.OCMode     = TIM_OCMODE_TOGGLE;
-         sConfig.OCPolarity = TIM_OCPOLARITY_HIGH;
-         sConfig.Pulse      = aCCValue_Buffer[0];
-         if(HAL_TIM_OC_ConfigChannel(&TimHandle, &sConfig, TIM_CHANNEL_1) != HAL_OK)
-         {
-           /* Configuration Error */
-           Error_Handler();
-         }
+	/*##-2- Configure the PWM channel 3 ########################################*/
+	sConfig.OCMode = TIM_OCMODE_TOGGLE;
+	sConfig.OCPolarity = TIM_OCPOLARITY_LOW;
+	sConfig.Pulse = 0;//aCCValue_Buffer[0];
+	if (HAL_TIM_OC_ConfigChannel(&TimHandle, &sConfig, TIM_CHANNEL_1)
+			!= HAL_OK) {
+		/* Configuration Error */
+		Error_Handler();
+	}
 
-         /*##-3- Start PWM signal generation in DMA mode ############################*/
-         if(  HAL_TIM_OC_Start_DMA(&TimHandle, TIM_CHANNEL_1, aCCValue_Buffer, 3) != HAL_OK)
-         {
-           /* Starting PWM generation Error */
-           Error_Handler();
-         }
-
+	/*##-3- Start PWM signal generation in DMA mode ############################*/
+	if (HAL_TIM_OC_Start_DMA(&TimHandle, TIM_CHANNEL_1, aCCValue_Buffer, 5)	!= HAL_OK) {
+		/* Starting PWM generation Error */
+		Error_Handler();
+	}
 
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+	while (1) {
   /* USER CODE END WHILE */
 
   /* USER CODE BEGIN 3 */
 
-  }
+	}
   /* USER CODE END 3 */
 
 }
@@ -175,10 +175,13 @@ void SystemClock_Config(void)
 
     /**Initializes the CPU, AHB and APB busses clocks 
     */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = 16;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
   if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
@@ -188,12 +191,12 @@ void SystemClock_Config(void)
     */
   RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
                               |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
   RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
 
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
   {
     _Error_Handler(__FILE__, __LINE__);
   }
@@ -307,10 +310,9 @@ static void MX_GPIO_Init(void)
 void _Error_Handler(char * file, int line)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  while(1) 
-  {
-  }
+	/* User can add his own implementation to report the HAL error return state */
+	while (1) {
+	}
   /* USER CODE END Error_Handler_Debug */ 
 }
 
@@ -326,8 +328,8 @@ void _Error_Handler(char * file, int line)
 void assert_failed(uint8_t* file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
-  /* User can add his own implementation to report the file name and line number,
-    ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	/* User can add his own implementation to report the file name and line number,
+	 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
   /* USER CODE END 6 */
 
 }
